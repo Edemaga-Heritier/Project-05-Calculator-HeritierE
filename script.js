2a175eda887e0c07aa8288a3f979378611ee22e0
herithieredemaga <hedemaga@gmail.com>
2023-08-17 21:09:25 +0100

    add structure

--- script.js
+++ script.js
@@ -1,3 +1,272 @@
-// { calculate } import './calculator';
-//Comment
-// TODO: Faire la manipulation du DOM dans ce fichier
+const bottomInput = document.getElementById('input');
+const upperLabel = document.getElementById('calcul');
+
+const digits = document.getElementsByClassName('digit');
+const operators = document.querySelectorAll('#minus, #times, #divideby, #plus');
+const clearButton = document.getElementById('clear');
+const resetButton = document.getElementById('reset');
+const dotButton = document.querySelector(".dot");
+const changeSignButton = document.getElementById('plusoumoins');
+const equalsButton = document.getElementById('equals');
+const percentageButton = document.getElementById('percentage')
+
+
+upperLabel.style.maxWidth = "400px";
+upperLabel.style.margin = "auto";
+
+let operationInput ="";
+let operationState = false;
+// Changement de type des boutons d'opération et sur le bouton d'égalité
+let newOperators = [];
+for (let i = 0; i < operators.length; i++) {
+    const element = operators[i];
+    element.type = "button";
+    newOperators.push(element);
+}
+equalsButton.type = 'button';
+//console.log(newOperators[0].type);
+percentageButton.type = 'button';
+
+// Sélection des codes clavier des touches du pavé numérique uniquement
+let numpadDigits = [];
+for (let index = 96; index < 112; index++) {
+    numpadDigits.push(index);
+}
+numpadDigits.push(8);
+
+// FONCTIONS D'EVENEMENTS
+function addDigit() {
+    if (operationState) {
+        upperLabel.innerText = "";
+        bottomInput.value = "";
+        if (bottomInput.value.length < 10) {
+            bottomInput.value += this.innerText;    
+        }else{
+            bottomInput.value   
+        }
+        operationState = false;
+    }else{
+        if (bottomInput.value.length < 10) {
+            bottomInput.value += this.innerText;    
+        }else{
+            bottomInput.value   
+        }
+        operationState = false;
+    }
+    
+}
+
+function addToLabel() {
+    let newValue = Array.from(bottomInput.value);
+    if (bottomInput.value.includes('.') && newValue.at(0) ==='.') {
+        newValue.unshift('0');
+        upperLabel.innerText +=  ' ' + newValue.join('') + ' ' + this.innerText;
+        bottomInput.value = "";
+    }else{
+        upperLabel.innerText += ' ' + bottomInput.value + ' ' + this.innerText;
+        bottomInput.value = "";
+    }
+}
+
+function makeCalculation(){
+    if (bottomInput.value === "") {
+        if (upperLabel.innerText.includes('×')) {
+            operationInput = upperLabel.innerText.split('×');
+            operationInput = operationInput.join('*');
+            if (operationInput.at(-1) === '*') {
+                operationInput = operationInput.substring(0, operationInput.length -1)
+                bottomInput.value = eval(operationInput);
+                operationState = true;
+            }else{
+                eval(operationInput);
+                operationState = true;
+
+            }
+        }else if (upperLabel.innerText.includes('÷')){
+            operationInput = upperLabel.innerText.split('÷');
+            operationInput = operationInput.join('/');
+            if (operationInput.at(-1) === '/') {
+                operationInput = operationInput.substring(0, operationInput.length -1)
+                bottomInput.value = eval(operationInput);
+                operationState = true; 
+
+            }else{
+                bottomInput.value = eval(operationInput);
+                operationState = true; 
+        
+            }
+        }else if (upperLabel.innerText.includes('+') || upperLabel.innerText.includes('-')){
+            operationInput = upperLabel.innerText;
+            if (operationInput.at(-1) === "+" || operationInput.at(-1) === "-") {
+                operationInput = operationInput.substring(0, operationInput.length -1);
+            }
+            bottomInput.value = eval(operationInput);
+            operationState = true; 
+
+        } 
+    }else{
+        upperLabel.innerText += ' ' + bottomInput.value;
+        if (upperLabel.innerText.includes('×')) {
+            operationInput = upperLabel.innerText.split('×');
+            operationInput = operationInput.join('*');
+            bottomInput.value = eval(operationInput);
+            upperLabel.innerText +=  ' =';
+        }else if (upperLabel.innerText.includes('÷')){
+            operationInput = upperLabel.innerText.split('÷');
+            operationInput = operationInput.join('/');
+            bottomInput.value = eval(operationInput);
+            upperLabel.innerText +=  ' =';
+            operationState = true;
+        }else if (upperLabel.innerText.includes('+') || upperLabel.innerText.includes('-')){
+            operationInput = upperLabel.innerText;
+            bottomInput.value = eval(operationInput);
+            upperLabel.innerText += ' =';
+            operationState = true; 
+        }
+    }
+    operationState = true;
+}
+
+function makePercentage() {
+    if (bottomInput.value === "") {
+        if (upperLabel.innerText.includes('×')) {
+            operationInput = upperLabel.innerText.split('×');
+            operationInput = operationInput.join('*');
+            if (operationInput.at(-1) === '*') {
+                operationInput = operationInput.substring(0, operationInput.length -1)
+                bottomInput.value = eval(operationInput) / 100;
+            }else{
+                eval(operationInput);
+            }
+        }else if (upperLabel.innerText.includes('÷')){
+            operationInput = upperLabel.innerText.split('÷');
+            operationInput = operationInput.join('/');
+            if (operationInput.at(-1) === '/') {
+                operationInput = operationInput.substring(0, operationInput.length -1)
+                bottomInput.value = eval(operationInput) / 100;
+            }else{
+                bottomInput.value = eval(operationInput) / 100;
+            }
+        }else if (upperLabel.innerText.includes('+') || upperLabel.innerText.includes('-')){
+            operationInput = upperLabel.innerText;
+            if (operationInput.at(-1) === "+" || operationInput.at(-1) === "-") {
+                operationInput = operationInput.substring(0, operationInput.length -1);
+            }
+            bottomInput.value = eval(operationInput) / 100;
+        } 
+    }else{
+        upperLabel.innerText += ' ' + bottomInput.value;
+        if (upperLabel.innerText.includes('×')) {
+            operationInput = upperLabel.innerText.split('×');
+            operationInput = operationInput.join('*');
+            bottomInput.value = eval(operationInput) / 100;
+            upperLabel.innerText +=  '÷ 100 = ';
+        }else if (upperLabel.innerText.includes('÷')){
+            operationInput = upperLabel.innerText.split('÷');
+            operationInput = operationInput.join('/');
+            bottomInput.value = eval(operationInput) / 100;
+            upperLabel.innerText +=  '÷ 100 = ';
+        }else if (upperLabel.innerText.includes('+') || upperLabel.innerText.includes('-')){
+            operationInput = upperLabel.innerText;
+            bottomInput.value = eval(operationInput) / 100;
+            upperLabel.innerText += '÷ 100 = ' 
+        }else{
+            bottomInput.value = eval(bottomInput.value+ "/100");
+        }
+    }
+    operationState = true;
+    
+}
+
+function addDot() {
+    if (bottomInput.value==="") {
+        bottomInput.value += this.innerText;
+    }else if (bottomInput.value.includes('.')){
+        bottomInput.value;
+    }else{
+        bottomInput.value += '.';
+    }
+}
+
+function clickZero() {
+    if (bottomInput.value === "") {
+        bottomInput.value = "0";
+    }else if (bottomInput.value ==="0"){
+        bottomInput.value;
+
+    }else{
+        if (bottomInput.value.length < 10) {
+            bottomInput.value += this.innerText;    
+        }
+        
+    }
+}
+
+function changeSign() {
+    let valeur = Array.from(bottomInput.value);
+    
+    if (!bottomInput.value) {
+        bottomInput.value;
+    }else if (bottomInput.value && valeur.at(0)!=="-"){
+        valeur.unshift("-");
+        bottomInput.value =(valeur.join(""));
+    }else if (bottomInput.value && valeur.at(0)==="-"){
+        valeur.shift();
+        bottomInput.value=(valeur.join("")) ;
+    }else{
+        bottomInput.value;
+    }
+}
+
+function returnPercentage() {
+    
+}
+
+function reset() {
+    upperLabel.innerText = "";
+    bottomInput.value = "";
+}
+
+function clearInput() {
+    bottomInput.value = "";
+}
+
+function blockKey(event) {
+    let keyCode = event.keyCode;
+    if (!numpadDigits.includes(keyCode)) {
+        event.preventDefault();
+    }
+}
+
+// AJOUT DES EVENEMENTS
+
+// sur les boutons des nombres
+for (let index = 0; index < digits.length; index++) {
+    const element = digits[index];
+    if (element.innerText === "0") {
+        element.addEventListener('click', clickZero);
+    }else{
+        element.addEventListener('click', addDigit)
+    }
+    
+}
+//sur les opérateurs
+for (let i = 0; i < operators.length; i++) {
+    const element = operators[i];
+    element.addEventListener('click', addToLabel);
+    
+}
+// sur l'input
+bottomInput.addEventListener('keydown', blockKey)
+// sur le bouton d'effacement
+clearButton.addEventListener('click',clearInput);
+// sur le bouton de réinitialisation
+resetButton.addEventListener('click', reset); 
+// sur le bouton de changement de signe
+changeSignButton.addEventListener('click', changeSign);
+// sur le bouton point
+dotButton.addEventListener('click', addDot);
+//sur le bouton d'égalité
+equalsButton.addEventListener('click', makeCalculation);
+// sur le bouton de pourcentage
+percentageButton.addEventListener('click', makePercentage)
